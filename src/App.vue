5c948415468dd7fafb9bbae8b07f99ec6667c3f8
Kerolt <1517482303@qq.com>
2024-07-07 15:21:34 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts"></script>
+
+<template>
+  <router-view />
+</template>
+
+<style>
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+  font-weight: normal;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+}
+
+::-webkit-scrollbar {
+  display: none;
+}
+</style>
