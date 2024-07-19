3bce8a4cd29af5f68d61b2b00587a3adb0dd6748
Kerolt <1517482303@qq.com>
2024-07-19 13:06:51 +0800

    添加命令：history

---
+++ src/commands/terminal/history-cmd.ts
@@ -0,0 +1,17 @@
+import { CommandType } from "../command";
+
+const historyCommand: CommandType = {
+  cmd: "history",
+  name: "历史记录",
+  alias: ["h"],
+  desc: "查看历史记录",
+  options: [],
+  action: (options, terminal) => {
+    const historys = terminal.listCommandHistory();
+    historys.forEach((history, index) => {
+      terminal.writeTextToResult(`${index + 1} ${history.text}`);
+    });
+  }
+};
+
+export default historyCommand;
