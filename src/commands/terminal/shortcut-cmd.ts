d1b22f6fa189568658b079bb511deffd86d34cf6
Kerolt <1517482303@qq.com>
2024-07-19 13:07:03 +0800

    添加命令：shortcut

---
+++ src/commands/terminal/shortcut-cmd.ts
@@ -0,0 +1,20 @@
+import { shortcutList } from "../../components/terminal/shortcut";
+import { CommandType } from "../command";
+
+const shortcutCommand: CommandType = {
+  cmd: "shortcut",
+  name: "快捷键",
+  desc: "查看快捷键",
+  options: [],
+  action: (options, terminal) => {
+    terminal.writeTextToResult("快捷键");
+    const shortcuts = shortcutList;
+    shortcuts.forEach((shortcut) => {
+      if (shortcut?.keyDesc) {
+        terminal.writeTextToResult(`${shortcut.keyDesc} ${shortcut.desc}`);
+      }
+    });
+  }
+};
+
+export default shortcutCommand;
